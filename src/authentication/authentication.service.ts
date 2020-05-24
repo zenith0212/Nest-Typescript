3d391dff432aa56935510021c9985cfbfe50e76d
zenith1994 <versatile.skydev@gmail.com>
2020-05-25 00:11:40 +0200

    feat(Auth): create the JwtStrategy that reads JWT token from the cookies

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -35,7 +35,7 @@ export class AuthenticationService {
   public getCookieWithJwtToken(userId: number) {
     const payload: TokenPayload = { userId };
     const token = this.jwtService.sign(payload);
-    return `Authorization=${token}; HttpOnly; Path=/; Max-Age=${this.configService.get('JWT_EXPIRATION_TIME')}`;
+    return `Authentication=${token}; HttpOnly; Path=/; Max-Age=${this.configService.get('JWT_EXPIRATION_TIME')}`;
   }
 
   public async getAuthenticatedUser(email: string, plainTextPassword: string) {
