e2c7b84d3cc2efa86af6646d0568a2271066024f
zenith1994 <versatile.skydev@gmail.com>
2020-05-31 19:30:41 +0200

    feat(Posts): validate id in the params

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,6 +3,7 @@ import PostsService from './posts.service';
 import CreatePostDto from './dto/createPost.dto';
 import UpdatePostDto from './dto/updatePost.dto';
 import JwtAuthenticationGuard from '../authentication/jwt-authentication.guard';
+import FindOneParams from '../utils/findOneParams';
 
 @Controller('posts')
 export default class PostsController {
@@ -16,7 +17,7 @@ export default class PostsController {
   }
 
   @Get(':id')
-  getPostById(@Param('id') id: string) {
+  getPostById(@Param() { id }: FindOneParams) {
     return this.postsService.getPostById(Number(id));
   }
 
@@ -27,12 +28,12 @@ export default class PostsController {
   }
 
   @Patch(':id')
-  async updatePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
+  async updatePost(@Param() { id }: FindOneParams, @Body() post: UpdatePostDto) {
     return this.postsService.updatePost(Number(id), post);
   }
 
   @Delete(':id')
-  async deletePost(@Param('id') id: string) {
+  async deletePost(@Param() { id }: FindOneParams) {
     return this.postsService.deletePost(Number(id));
   }
 }
