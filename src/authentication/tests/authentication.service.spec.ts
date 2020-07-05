890e6ae8f5601aca6f5cd1dcef4c7739031d02af
zenith1994 <versatile.skydev@gmail.com>
2020-07-05 22:29:36 +0200

    test(Authentication): mock the ConfigService and JwtService

--- src/authentication/tests/authentication.service.spec.ts
+++ src/authentication/tests/authentication.service.spec.ts
@@ -1,50 +1,36 @@
 import { AuthenticationService } from '../authentication.service';
 import { Test } from '@nestjs/testing';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
-import * as Joi from '@hapi/joi';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import User from '../../users/user.entity';
 import { UsersService } from '../../users/users.service';
+import mockedJwtService from '../../utils/mocks/jwt.service';
+import mockedConfigService from '../../utils/mocks/config.service';
 
 describe('The AuthenticationService', () => {
   let authenticationService: AuthenticationService;
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          validationSchema: Joi.object({
-            POSTGRES_HOST: Joi.string().required(),
-            POSTGRES_PORT: Joi.number().required(),
-            POSTGRES_USER: Joi.string().required(),
-            POSTGRES_PASSWORD: Joi.string().required(),
-            POSTGRES_DB: Joi.string().required(),
-            JWT_SECRET: Joi.string().required(),
-            JWT_EXPIRATION_TIME: Joi.string().required(),
-            PORT: Joi.number(),
-          })
-        }),
-        JwtModule.registerAsync({
-          imports: [ConfigModule],
-          inject: [ConfigService],
-          useFactory: async (configService: ConfigService) => ({
-            secret: configService.get('JWT_SECRET'),
-            signOptions: {
-              expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
-            },
-          }),
-        }),
-      ],
       providers: [
         UsersService,
         AuthenticationService,
         {
+          provide: ConfigService,
+          useValue: mockedConfigService
+        },
+        {
+          provide: JwtService,
+          useValue: mockedJwtService
+        },
+        {
           provide: getRepositoryToken(User),
-          useValue: {},
+          useValue: {}
         }
       ],
-    }).compile();
-    authenticationService = await module.get<AuthenticationService>(AuthenticationService);
+    })
+      .compile();
+    authenticationService = await module.get(AuthenticationService);
   })
   describe('when creating a cookie', () => {
     it('should return a string', () => {
