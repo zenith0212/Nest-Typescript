191b3561eb2c6d9262281f574c4907d7c0e8af64
zenith1994 <versatile.skydev@gmail.com>
2020-11-29 19:45:54 +0100

    feat(Subscribers): create the Subscriber service

--- src/subscribers/subscribers.service.interface.ts
+++ src/subscribers/subscribers.service.interface.ts
@@ -1,8 +1,9 @@
 import CreateSubscriberDto from './dto/createSubscriber.dto';
+import Subscriber from './subscriber.service';
 
 interface SubscribersService {
-  addSubscriber(subscriber: CreateSubscriberDto): Promise<any>
-  getAllSubscribers(params: {}): Promise<any>
+  addSubscriber(subscriber: CreateSubscriberDto): Promise<Subscriber>
+  getAllSubscribers(params: {}): Promise<Subscriber>
 }
 
 export default SubscribersService;
