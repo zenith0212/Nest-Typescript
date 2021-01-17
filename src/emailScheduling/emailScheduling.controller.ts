0c4d0e2c2635cfe2cc18b5e8de9311d465c613f6
zenith1994 <versatile.skydev@gmail.com>
2021-01-17 21:41:56 +0100

    feat(EmailScheduling): create the basics of the EmailSchedulingController

---
+++ src/emailScheduling/emailScheduling.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Body,
+  Controller,
+  UseGuards,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  Post,
+} from '@nestjs/common';
+import JwtAuthenticationGuard from '../authentication/jwt-authentication.guard';
+import EmailSchedulingService from './emailScheduling.service';
+import EmailScheduleDto from './dto/emailSchedule.dto';
+
+@Controller('email-scheduling')
+@UseInterceptors(ClassSerializerInterceptor)
+export default class EmailSchedulingController {
+  constructor(
+    private readonly emailSchedulingService: EmailSchedulingService
+  ) {}
+
+  @Post('schedule')
+  @UseGuards(JwtAuthenticationGuard)
+  async scheduleEmail(@Body() emailSchedule: EmailScheduleDto) {
+    await this.emailSchedulingService.scheduleEmail(emailSchedule);
+  }
+}
--- src/emailScheduling/emailScheduling.service.ts
+++ src/emailScheduling/emailScheduling.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import EmailService from '../email/email.service';
+import EmailScheduleDto from './dto/emailSchedule.dto';
 
 @Injectable()
 export default class EmailSchedulingService {
   constructor(
     private readonly emailService: EmailService
   ) {}
+
+  scheduleEmail(emailSchedule: EmailScheduleDto) {
+    return this.emailService.sendMail({
+      to: emailSchedule.recipient,
+      subject: emailSchedule.subject,
+      text: emailSchedule.content
+    })
+  }
 }
