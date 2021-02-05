972d43388ec9e70d0d6e19d53df37bc91a6fcc71
zenith1994 <versatile.skydev@gmail.com>
2021-02-05 22:14:42 +0100

    feat(Posts): add a mutation for creating a post

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,6 +1,10 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Post } from './models/post.model';
 import PostsService from './posts.service';
+import { CreatePostInput } from './inputs/post.input';
+import { UseGuards } from '@nestjs/common';
+import RequestWithUser from '../authentication/requestWithUser.interface';
+import { GraphqlJwtAuthGuard } from '../authentication/graphql-jwt-auth.guard';
 
 @Resolver(() => Post)
 export class PostsResolver {
@@ -12,5 +16,14 @@ export class PostsResolver {
   async posts() {
     const posts = await this.postsService.getAllPosts();
     return posts.items;
+  }
+
+  @Mutation(() => Post)
+  @UseGuards(GraphqlJwtAuthGuard)
+  async createPost(
+    @Args('input') createPostInput: CreatePostInput,
+    @Context() context: { req: RequestWithUser },
+  ) {
+    return this.postsService.createPost(createPostInput, context.req.user);
   }
 }
