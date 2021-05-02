08aca70f054516ba9a17982a35e48fe22b50b5dc
zenith1994 <versatile.skydev@gmail.com>
2021-05-02 15:32:07 +0200

    feat(Optimize): run the image processing in a separate process

--- src/optimize/image.processor.ts
+++ src/optimize/image.processor.ts
@@ -1,36 +1,33 @@
-import { Process, Processor } from '@nestjs/bull';
-import { Job } from 'bull';
 import * as AdmZip from 'adm-zip';
 import { buffer } from 'imagemin';
 import imageminPngquant from 'imagemin-pngquant';
 import { Express } from 'express';
+import { Job, DoneCallback } from 'bull';
 
-@Processor('image')
-export class ImageProcessor {
-  @Process('optimize')
-  async handleOptimization(job: Job) {
-    const files: Express.Multer.File[] = job.data.files;
+async function imageProcessor(job: Job, doneCallback: DoneCallback) {
+  const files: Express.Multer.File[] = job.data.files;
 
-    const optimizationPromises: Promise<Buffer>[] = files.map(file => {
-      const fileBuffer = Buffer.from(file.buffer);
-      return buffer(fileBuffer, {
-        plugins: [
-          imageminPngquant({
-            quality: [0.6, 0.8]
-          })
-        ]
-      })
+  const optimizationPromises: Promise<Buffer>[] = files.map(file => {
+    const fileBuffer = Buffer.from(file.buffer);
+    return buffer(fileBuffer, {
+      plugins: [
+        imageminPngquant({
+          quality: [0.6, 0.8]
+        })
+      ]
     })
+  })
 
-    const optimizedImages = await Promise.all(optimizationPromises);
+  const optimizedImages = await Promise.all(optimizationPromises);
 
-    const zip = new AdmZip();
+  const zip = new AdmZip();
 
-    optimizedImages.forEach((image, index) => {
-      const fileData = files[index];
-      zip.addFile(fileData.originalname, image);
-    })
+  optimizedImages.forEach((image, index) => {
+    const fileData = files[index];
+    zip.addFile(fileData.originalname, image);
+  })
 
-    return zip.toBuffer();
-  }
+  doneCallback(null, zip.toBuffer());
 }
+
+export default imageProcessor;
--- src/optimize/optimize.module.ts
+++ src/optimize/optimize.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { OptimizeController } from './optimize.controller';
 import { BullModule } from '@nestjs/bull';
-import { ImageProcessor } from './image.processor';
+import { join } from 'path';
 
 @Module({
   imports: [
     BullModule.registerQueue({
       name: 'image',
+      processors: [{
+        name: 'optimize',
+        path: join(__dirname, 'image.processor.js')
+      }],
     })
   ],
-  providers: [ImageProcessor],
+  providers: [],
   exports: [],
   controllers: [OptimizeController]
 })
